3027bbd8ee2556e88b353b81a6a02441bafe1ea1
mina <minaraafatemil14@gmail.com>
2024-10-23 13:39:40 +0300

    apply form styling

--- client/src/components/SpecialRequest.jsx
+++ client/src/components/SpecialRequest.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export default function SpecialRequestForm() {
   const [form, setForm] = useState({
-    name: "",
-    message: "",
-    phoneNumber: "", // Add phoneNumber to the form state
+    name: '',
+    message: '',
+    phoneNumber: '',
   });
-  const [image, setImage] = useState(null); // State for the uploaded image
+  const [image, setImage] = useState(null);
   const [submitSuccess, setSubmitSuccess] = useState(false);
 
   function updateForm(value) {
@@ -15,122 +15,152 @@ export default function SpecialRequestForm() {
     });
   }
 
-  // Handle image upload
   function handleImageChange(e) {
     setImage(e.target.files[0]);
   }
-
   async function onSubmit(e) {
     e.preventDefault();
 
-    // Create FormData to send the image along with the other form data
     const formData = new FormData();
-    formData.append("name", form.name);
-    formData.append("message", form.message);
-    formData.append("phoneNumber", form.phoneNumber); // Append the phone number
+    formData.append('name', form.name);
+    formData.append('message', form.message);
+    formData.append('phoneNumber', form.phoneNumber);
     if (image) {
-      formData.append("imageSrc", image); // Append the image file
+      formData.append('imageSrc', image);
+    }
+
+    // Log the FormData
+    for (const [key, value] of formData.entries()) {
+      console.log(key, value);
     }
 
     try {
-      const response = await fetch("http://localhost:5050/special-request", {
-        method: "POST",
-        body: formData, // Send formData, not JSON
+      const response = await fetch('http://localhost:5050/specialRequests', {
+        method: 'POST',
+        body: formData,
       });
 
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
 
-      // If the request was successful, you can show a success message
       setSubmitSuccess(true);
-      console.log("Special request sent:", await response.json());
+      console.log('Special request sent:', await response.json());
     } catch (error) {
-      console.error("A problem occurred while sending the request: ", error);
+      console.error('A problem occurred while sending the request: ', error);
     } finally {
-      setForm({ name: "", message: "", phoneNumber: "" }); // Reset the form
-      setImage(null); // Reset the image
+      setForm({ name: '', message: '', phoneNumber: '' });
+      setImage(null);
     }
   }
 
   return (
-    <>
-      <h3 className="text-lg font-semibold p-4">Special Request Form</h3>
-      <form onSubmit={onSubmit} className="border rounded-lg overflow-hidden p-4" encType="multipart/form-data">
-        <div className="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-slate-900/10 pb-12 md:grid-cols-2">
-          <div>
-            <h2 className="text-base font-semibold leading-7 text-slate-900">Request Info</h2>
-            <p className="mt-1 text-sm leading-6 text-slate-600">
-              Please provide your details and any special requests you have.
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md">
+        <h3 className="text-lg font-semibold p-4 text-center">
+          Special Request Form
+        </h3>
+        <form
+          onSubmit={onSubmit}
+          className="border rounded-lg shadow-lg overflow-hidden p-6 bg-white"
+        >
+          <div className="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-slate-900/10 pb-12 md:grid-cols-2 mx-auto">
+            <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 mx-auto">
+              <div className="sm:col-span-4">
+                <label
+                  htmlFor="name"
+                  className="block text-sm font-medium leading-6 text-slate-900"
+                >
+                  Name
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    className="block flex-1 border border-gray-300 rounded-md py-2 px-3 focus:ring focus:ring-indigo-500 focus:border-indigo-500"
+                    placeholder="Your Name"
+                    value={form.name}
+                    onChange={(e) => updateForm({ name: e.target.value })}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-4">
+                <label
+                  htmlFor="phoneNumber"
+                  className="block text-sm font-medium leading-6 text-slate-900"
+                >
+                  Phone Number
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="tel"
+                    name="phoneNumber"
+                    id="phoneNumber"
+                    className="block flex-1 border border-gray-300 rounded-md py-2 px-3 focus:ring focus:ring-indigo-500 focus:border-indigo-500"
+                    placeholder="Your Phone Number"
+                    value={form.phoneNumber}
+                    onChange={(e) =>
+                      updateForm({ phoneNumber: e.target.value })
+                    }
+                    required
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-4">
+                <label
+                  htmlFor="message"
+                  className="block text-sm font-medium leading-6 text-slate-900"
+                >
+                  Message
+                </label>
+                <div className="mt-2">
+                  <textarea
+                    name="message"
+                    id="message"
+                    className="block flex-1 border border-gray-300 rounded-md py-2 px-3 focus:ring focus:ring-indigo-500 focus:border-indigo-500"
+                    placeholder="Your Message or Special Request"
+                    value={form.message}
+                    onChange={(e) => updateForm({ message: e.target.value })}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-4">
+                <label
+                  htmlFor="image"
+                  className="block text-sm font-medium leading-6 text-slate-900"
+                >
+                  Upload Image (optional)
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="file"
+                    name="image"
+                    id="image"
+                    className="block w-full text-sm text-gray-500 border border-gray-300 rounded-md py-2 px-3 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100"
+                    accept="image/*"
+                    onChange={handleImageChange}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-center mt-4">
+            <input
+              type="submit"
+              value="Send Request"
+              className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 rounded-md h-10 px-4 transition duration-200"
+            />
+          </div>
+          {submitSuccess && (
+            <p className="mt-2 text-green-600 text-center">
+              Your special request has been submitted!
             </p>
-          </div>
-          <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8">
-            <div className="sm:col-span-4">
-              <label htmlFor="name" className="block text-sm font-medium leading-6 text-slate-900">Name</label>
-              <div className="mt-2">
-                <input
-                  type="text"
-                  name="name"
-                  id="name"
-                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-slate-900 placeholder:text-slate-400 focus:ring-0 sm:text-sm sm:leading-6"
-                  placeholder="Your Name"
-                  value={form.name}
-                  onChange={(e) => updateForm({ name: e.target.value })}
-                  required
-                />
-              </div>
-            </div>
-            <div className="sm:col-span-4">
-              <label htmlFor="phoneNumber" className="block text-sm font-medium leading-6 text-slate-900">Phone Number</label>
-              <div className="mt-2">
-                <input
-                  type="tel"
-                  name="phoneNumber"
-                  id="phoneNumber"
-                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-slate-900 placeholder:text-slate-400 focus:ring-0 sm:text-sm sm:leading-6"
-                  placeholder="Your Phone Number"
-                  value={form.phoneNumber}
-                  onChange={(e) => updateForm({ phoneNumber: e.target.value })}
-                  required
-                />
-              </div>
-            </div>
-            <div className="sm:col-span-4">
-              <label htmlFor="message" className="block text-sm font-medium leading-6 text-slate-900">Message</label>
-              <div className="mt-2">
-                <textarea
-                  name="message"
-                  id="message"
-                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-slate-900 placeholder:text-slate-400 focus:ring-0 sm:text-sm sm:leading-6"
-                  placeholder="Your Message or Special Request"
-                  value={form.message}
-                  onChange={(e) => updateForm({ message: e.target.value })}
-                  required
-                />
-              </div>
-            </div>
-            <div className="sm:col-span-4">
-              <label htmlFor="image" className="block text-sm font-medium leading-6 text-slate-900">Upload Image (optional)</label>
-              <div className="mt-2">
-                <input
-                  type="file"
-                  name="image"
-                  id="image"
-                  className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100"
-                  accept="image/*" // Restrict to image files
-                  onChange={handleImageChange}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-        <input
-          type="submit"
-          value="Send Request"
-          className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 hover:text-accent-foreground h-9 rounded-md px-3 cursor-pointer mt-4"
-        />
-        {submitSuccess && <p className="mt-2 text-green-600">Your special request has been submitted!</p>}
-      </form>
-    </>
+          )}
+        </form>
+      </div>
+    </div>
   );
 }
--- client/src/components/admin/Record.jsx
+++ client/src/components/admin/Record.jsx
@@ -1,16 +1,16 @@
-import { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 
 export default function RecordForm() {
   const [form, setForm] = useState({
-    name: "",
-    price: "",
-    id: "",
-    imageSrc: "",
-    description: "",
-    category: "",
+    name: '',
+    price: '',
+    id: '',
+    imageSrc: '',
+    description: '',
+    category: '',
   });
-  const [file, setFile] = useState(null); // State for the image file
+  const [file, setFile] = useState(null);
   const [isNew, setIsNew] = useState(true);
   const params = useParams();
   const navigate = useNavigate();
@@ -28,7 +28,7 @@ export default function RecordForm() {
       const record = await response.json();
       if (!record) {
         console.warn(`Record with id ${id} not found`);
-        navigate("/");
+        navigate('/');
         return;
       }
 
@@ -55,22 +55,20 @@ export default function RecordForm() {
     formData.append('price', form.price);
     formData.append('description', form.description);
     formData.append('category', form.category);
-
-    // Append file only if there is a file
     if (file) {
-      formData.append('imageSrc', file); // The key should match what multer expects ('imageSrc')
+      formData.append('imageSrc', file);
     }
 
     try {
       let response;
       if (isNew) {
-        response = await fetch("http://localhost:5050/record", {
-          method: "POST",
-          body: formData, // No need for 'Content-Type', it's automatically set when using FormData
+        response = await fetch('http://localhost:5050/record', {
+          method: 'POST',
+          body: formData,
         });
       } else {
         response = await fetch(`http://localhost:5050/record/${params.id}`, {
-          method: "PATCH",
+          method: 'PATCH',
           body: formData,
         });
       }
@@ -82,99 +80,121 @@ export default function RecordForm() {
       const result = await response.json();
       console.log('Record saved:', result);
     } catch (error) {
-      console.error("A problem occurred adding or updating a record: ", error);
+      console.error('A problem occurred adding or updating a record: ', error);
     } finally {
-      setForm({ name: "", price: "", id: "", imageSrc: "", description: "", category: "" });
-      setFile(null); // Reset the file input
-      navigate("/");
+      setForm({
+        name: '',
+        price: '',
+        id: '',
+        imageSrc: '',
+        description: '',
+        category: '',
+      });
+      setFile(null);
+      navigate('/');
     }
   }
 
   return (
-    <>
-      <h3 className="text-lg font-semibold p-4">Create/Update Record</h3>
-      <form onSubmit={onSubmit} className="border rounded-lg overflow-hidden p-4">
-        <div className="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-slate-900/10 pb-12 md:grid-cols-2">
-          <div>
-            <h2 className="text-base font-semibold leading-7 text-slate-900">Record Info</h2>
-            <p className="mt-1 text-sm leading-6 text-slate-600">
-              This information will be displayed publicly, so be careful what you share.
-            </p>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md">
+        <h3 className="text-lg font-semibold p-4 text-center">
+          Create/Update Product
+        </h3>
+        <form onSubmit={onSubmit} className="border rounded-lg shadow-lg overflow-hidden p-6 bg-white">
+          <div className="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-slate-900/10 pb-12 md:grid-cols-2 mx-auto">
+            <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 mx-auto">
+              <div className="sm:col-span-4">
+                <label htmlFor="name" className="block text-sm font-medium leading-6 text-slate-900">
+                  Name
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    className="block flex-1 border border-gray-300 rounded-md py-2 px-3 focus:ring focus:ring-indigo-500 focus:border-indigo-500"
+                    placeholder="Product Name"
+                    value={form.name}
+                    onChange={(e) => updateForm({ name: e.target.value })}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-4">
+                <label htmlFor="price" className="block text-sm font-medium leading-6 text-slate-900">
+                  Price
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    name="price"
+                    id="price"
+                    className="block flex-1 border border-gray-300 rounded-md py-2 px-3 focus:ring focus:ring-indigo-500 focus:border-indigo-500"
+                    placeholder="Price"
+                    value={form.price}
+                    onChange={(e) => updateForm({ price: e.target.value })}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-4">
+                <label htmlFor="imageSrc" className="block text-sm font-medium leading-6 text-slate-900">
+                  Image Upload
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="file"
+                    accept="image/*"
+                    onChange={(e) => setFile(e.target.files[0])}
+                    className="block w-full text-sm text-gray-500 border border-gray-300 rounded-md py-2 px-3 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-4">
+                <label htmlFor="description" className="block text-sm font-medium leading-6 text-slate-900">
+                  Description
+                </label>
+                <div className="mt-2">
+                  <textarea
+                    name="description"
+                    id="description"
+                    className="block flex-1 border border-gray-300 rounded-md py-2 px-3 focus:ring focus:ring-indigo-500 focus:border-indigo-500"
+                    placeholder="Product Description"
+                    value={form.description}
+                    onChange={(e) => updateForm({ description: e.target.value })}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-4">
+                <label htmlFor="category" className="block text-sm font-medium leading-6 text-slate-900">
+                  Category
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    name="category"
+                    id="category"
+                    className="block flex-1 border border-gray-300 rounded-md py-2 px-3 focus:ring focus:ring-indigo-500 focus:border-indigo-500"
+                    placeholder="Category"
+                    value={form.category}
+                    onChange={(e) => updateForm({ category: e.target.value })}
+                    required
+                  />
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8">
-            <div className="sm:col-span-4">
-              <label htmlFor="name" className="block text-sm font-medium leading-6 text-slate-900">Name</label>
-              <div className="mt-2">
-                <input
-                  type="text"
-                  name="name"
-                  id="name"
-                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-slate-900 placeholder:text-slate-400 focus:ring-0 sm:text-sm sm:leading-6"
-                  placeholder="Record Name"
-                  value={form.name}
-                  onChange={(e) => updateForm({ name: e.target.value })}
-                />
-              </div>
-            </div>
-            <div className="sm:col-span-4">
-              <label htmlFor="price" className="block text-sm font-medium leading-6 text-slate-900">Price</label>
-              <div className="mt-2">
-                <input
-                  type="text"
-                  name="price"
-                  id="price"
-                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-slate-900 placeholder:text-slate-400 focus:ring-0 sm:text-sm sm:leading-6"
-                  placeholder="Price"
-                  value={form.price}
-                  onChange={(e) => updateForm({ price: e.target.value })}
-                />
-              </div>
-            </div>
-            <div className="sm:col-span-4">
-              <label htmlFor="imageSrc" className="block text-sm font-medium leading-6 text-slate-900">Image Upload</label>
-              <div className="mt-2">
-                <input
-                  type="file"
-                  accept="image/*" // Optional: restrict to image files
-                  onChange={(e) => setFile(e.target.files[0])}
-                />
-              </div>
-            </div>
-            <div className="sm:col-span-4">
-              <label htmlFor="description" className="block text-sm font-medium leading-6 text-slate-900">Description</label>
-              <div className="mt-2">
-                <textarea
-                  name="description"
-                  id="description"
-                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-slate-900 placeholder:text-slate-400 focus:ring-0 sm:text-sm sm:leading-6"
-                  placeholder="Record Description"
-                  value={form.description}
-                  onChange={(e) => updateForm({ description: e.target.value })}
-                />
-              </div>
-            </div>
-            <div className="sm:col-span-4">
-              <label htmlFor="category" className="block text-sm font-medium leading-6 text-slate-900">Category</label>
-              <div className="mt-2">
-                <input
-                  type="text"
-                  name="category"
-                  id="category"
-                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-slate-900 placeholder:text-slate-400 focus:ring-0 sm:text-sm sm:leading-6"
-                  placeholder="Category"
-                  value={form.category}
-                  onChange={(e) => updateForm({ category: e.target.value })}
-                />
-              </div>
-            </div>
+          <div className="flex justify-center mt-4">
+            <input
+              type="submit"
+              value="Save Record"
+              className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 rounded-md h-10 px-4 transition duration-200"
+            />
           </div>
-        </div>
-        <input
-          type="submit"
-          value="Save Record"
-          className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 hover:text-accent-foreground h-9 rounded-md px-3 cursor-pointer mt-4"
-        />
-      </form>
-    </>
+        </form>
+      </div>
+    </div>
   );
 }
